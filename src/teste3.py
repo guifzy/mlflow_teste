feb885e21240878507ad982cdd5e625f6129fe21
guifzy <guifrxx@gmail.com>
2026-02-19 22:00:35 -0300

    experimento com multiplos parametros

--- src/teste3.py
+++ src/teste3.py
@@ -2,13 +2,13 @@ import mlflow
 import mlflow.sklearn
 from sklearn.datasets import load_wine
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
-import os 
 from dotenv import load_dotenv
 import dagshub
+from sklearn.model_selection import GridSearchCV
 
 dagshub.init(repo_owner="guifzy", repo_name="mlflow_teste", mlflow=True)
 
@@ -22,23 +22,38 @@ y = wine.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
 
 # parametros do modelo
-max_depth = 12
-n_estimators = 6
+parametros = {
+    'max_depth': [5, 10, 15, None],
+    'n_estimators': [10, 50, 100, 200]
+}
 
-mlflow.set_experiment("Wine_Classification_teste2")
+mlflow.set_experiment("Wine_Classification_teste3")
 
-with mlflow.start_run():
-    model = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=42)
-    model.fit(X_train, y_train)
+with mlflow.start_run() as main_run:
+    kf = KFold(n_splits=5, shuffle=True, random_state=42)
+    grid = GridSearchCV(estimator=RandomForestClassifier(random_state=42), param_grid=parametros, cv=kf)
+    grid.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
+    for i in range(len(grid.cv_results_['params'])):
+
+        with mlflow.start_run(nested=True) as child_run:
+            mlflow.log_params(grid.cv_results_['params'][i])
+            mlflow.log_metric("accuracy", grid.cv_results_['mean_test_score'][i])
+
+    best_params = grid.best_params_
+    best_score = grid.best_score_
+
+    mlflow.log_params(best_params)
+    mlflow.log_metric("best_accuracy", best_score)
+
+    y_pred = grid.predict(X_test)
 
     acc = accuracy_score(y_test, y_pred)
     cm = confusion_matrix(y_test, y_pred)
 
     # Log dos parametros e metricas do modelo para acompanhamento e comparação futura
-    mlflow.log_param("max_depth", max_depth)
-    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("max_depth", best_params['max_depth'])
+    mlflow.log_param("n_estimators", best_params['n_estimators'])
     mlflow.log_metric("accuracy", acc)
     
     plt.figure(figsize=(8, 6))
@@ -53,7 +68,7 @@ with mlflow.start_run():
 
     # Log de tags para melhor organização e busca dos experimentos
     mlflow.set_tags({"model_type": "RandomForest", "dataset": "WineDataSet", "author": "Guilherme Monteiro", "project": "MLflow Teste"})
-    mlflow.sklearn.log_model(model, "random_forest_model") # log do modelo treinado
+    mlflow.sklearn.log_model(grid, "random_forest_model") # log do modelo treinado
     mlflow.log_artifact(__file__)  # Log do código fonte do test
 
     print(f"Accuracy: {acc}")
